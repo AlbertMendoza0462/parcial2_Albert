0188817200e80c2700bd01bbb4bc6412cd84fdc1
Albert Mendoza <albertmendoza0462@hotmail.com>
2022-11-02 19:12:11 -0400

    Archivo VerbosResponseDto

---
+++ app/src/main/java/edu/ucne/parcial2_albert/data/remote/dto/VerbosResponseDto.kt
@@ -0,0 +1,8 @@
+package edu.ucne.parcial2_albert.data.remote.dto
+
+data class VerbosResponseDto (
+    val Verbo: String="",
+    val Categoria: String="",
+    val Nivel: String="",
+    val Imagen: String=""
+)
