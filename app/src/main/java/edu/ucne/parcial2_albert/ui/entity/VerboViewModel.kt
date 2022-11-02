6d92e22eea34fac641a6297c3730e3a50fdefeaf
Albert Mendoza <albertmendoza0462@hotmail.com>
2022-11-02 19:14:05 -0400

    Archivo VerboViewModel

---
+++ app/src/main/java/edu/ucne/parcial2_albert/ui/entity/VerboViewModel.kt
@@ -0,0 +1,36 @@
+package edu.ucne.parcial2_albert.ui.entity
+
+import androidx.compose.runtime.*
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import edu.ucne.parcial2_albert.data.Repository.VerboApiRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.getAndUpdate
+import kotlinx.coroutines.launch
+import java.io.IOException
+import javax.inject.Inject
+
+@HiltViewModel
+class VerboViewModel @Inject constructor(
+    private val verbosRepository: VerboApiRepository
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(VerboState())
+    val uiState: StateFlow<VerboState> = _uiState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            _uiState.getAndUpdate {
+                try {
+                    it.copy(verbos = verbosRepository.getVerbos())
+                }catch (ioe: IOException){
+                    val mgs = ioe.message.toString()
+                    it.copy(error = mgs)
+                }
+            }
+        }
+    }
+}
